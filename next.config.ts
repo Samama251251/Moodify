1ee6323a0cd205d0e295a4fb24daf56a4fecef42
samama251251 <samama251251@gmail.com>
2026-04-25 18:20:02 +0500

    init: moodify project setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+const nextConfig: NextConfig = {}
+export default withPWA(nextConfig)
